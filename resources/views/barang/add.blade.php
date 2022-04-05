64383e8b5fc4489a5a791f67fe25492b316dcb4c
TheChosenOne29 <gilbertwuaten@gmail.com>
2022-04-05 23:55:42 +0700

    Added pages for CRUD

---
+++ resources/views/barang/add.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.navbar')
+
+@section('content')
+<div class="mb-3">
+    <input class="form-control" type="text" placeholder="Default input" aria-label="default input example">
+  </div>
+  <div class="mb-3">
+    <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+@endsection
